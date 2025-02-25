c37245545b8f40d20463977a2cab5b01342f2209
Lucas Freire Sêmeler <2023102060050@aluno.ifro.edu.br>
2025-02-24 20:51:14 -0400

    Corrigindo repositório para o meu GitHub

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: 'https://github.com/semeler/meu-portfolio.git', // Altere para o nome exato do repositório no GitHub
+  base: '/meu-portfolio/', // Altere para o nome exato do repositório no GitHub
 });

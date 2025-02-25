4933e592573dfd268bd4e7812df14d02f6892f9b
Lucas Freire Sêmeler <2023102060050@aluno.ifro.edu.br>
2025-02-24 20:35:18 -0400

    Primeiro commit

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/NOME_DO_REPOSITORIO/', // Altere para o nome do seu repositório
+  base: 'https://github.com/Semeler/meu-portfolio.git', // Altere para o nome do seu repositório
 });
